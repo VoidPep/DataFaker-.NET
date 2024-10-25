f805edaeb708be0264cd4f5e76ff5aaf5eea9bd8
voidpep <pedrohm1009@gmail.com>
2024-10-24 21:31:03 -0300

    #feat: Possibilidade de enviar um email diferente

--- DataFaker.Web/Controllers/HomeController.cs
+++ DataFaker.Web/Controllers/HomeController.cs
@@ -23,12 +23,12 @@ namespace DataFaker.Web.Controllers
             return View();
         }
 
-        public IActionResult Gerar([FromQuery] int quantidade)
+        public IActionResult Gerar([FromQuery] int? quantidade, [FromQuery] string? email)
         {
             quantidade = quantidade < 10 ? 10 : quantidade;
             quantidade = quantidade > 1000 ? 1000 : quantidade;
 
-            var stream = _generator.Generate(quantidade);
+            var stream = _generator.Generate(quantidade ?? 10);
 
             var anexo = new Anexo
             {
@@ -36,19 +36,16 @@ namespace DataFaker.Web.Controllers
                 ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
                 Nome = "dados_gerados.xlsx",
             };
-            try
-            {
-                var corpo = "Olá!\nSegue em anexo o arquivo com os dados gerados.\nAtenciosamente,\nPedro";
+            var corpo = "Olá!\nSegue em anexo o arquivo com os dados gerados.\nAtenciosamente,\nPedro";
 
-                var to = Environment.GetEnvironmentVariable("SMTP_TO") ?? "";
+            //Environment.GetEnvironmentVariable("SMTP_TO")
+            var to = email == null || email == "" ? "" : email;
 
-                _emailService.EnviarEmailAsync("", "DataFaker - Dados Gerados", corpo, anexo);
+            if (to == null || to == "")
                 return File(stream, anexo.ContentType, anexo.Nome);
-            }
-            catch (Exception e)
-            {
-                return File(stream, anexo.ContentType, anexo.Nome);
-            }
+
+            _emailService.EnviarEmailAsync("", "DataFaker - Dados Gerados", corpo, anexo);
+            return File(stream, anexo.ContentType, anexo.Nome);
         }
     }
 }
